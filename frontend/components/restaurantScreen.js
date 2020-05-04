49e1665ed2884d6e55b1f1a98b7d7ed47847a427
Prakort <leanprakort@gmail.com>
2020-05-04 15:07:48 -0400

    fake menu

--- frontend/components/restaurantScreen.js
+++ frontend/components/restaurantScreen.js
@@ -12,32 +12,76 @@ import {
   Tile,
 } from "react-native-elements";
 
-const fakeData = [
+const restaurants = [
   {
     name: `Aracta's Pizza`,
     menu: {
       pizza: [
         {
-        name: 'Hawaii',
-        description: 'Best pizza small:210 cal, med:300cal, ........',
-        sizes: [
-          {
-            size: 'S',
-            price: 10.99
-          },
-          {
-            size: 'M',
-            price: 12.99
-          },
-          {
-            size: 'L',
-            price: 14.99
-          },
-          {
-            size: 'XL',
-            price: 14.99
-          },
-        ]
+          name: 'Hawaii',
+          description: 'Best pizza small:210 cal, med:300cal, ........',
+          sizes: [
+            {
+              size: 'S',
+              price: 10.99
+            },
+            {
+              size: 'M',
+              price: 12.99
+            },
+            {
+              size: 'L',
+              price: 14.99
+            },
+            {
+              size: 'XL',
+              price: 14.99
+            },
+          ]
+        },
+        {
+          name: 'Cheese and Sauage',
+          description: 'Best pizza small:210 cal, med:300cal, ........',
+          sizes: [
+            {
+              size: 'S',
+              price: 10.99
+            },
+            {
+              size: 'M',
+              price: 12.99
+            },
+            {
+              size: 'L',
+              price: 14.99
+            },
+            {
+              size: 'XL',
+              price: 14.99
+            },
+          ]
+        },
+        {
+          name: 'Vegie',
+          description: 'Best pizza small:210 cal, lot and lot of vegie, ........',
+          sizes: [
+            {
+              size: 'S',
+              price: 10.99
+            },
+            {
+              size: 'M',
+              price: 12.99
+            },
+            {
+              size: 'L',
+              price: 14.99
+            },
+            {
+              size: 'XL',
+              price: 14.99
+            },
+          ]
         }
       ],
       desert: [
@@ -45,11 +89,44 @@ const fakeData = [
           name: 'Funnel Cake Stick',
           description: 'Sweet strips of delicious funnel cake made by our special in house chef. Fresh out of the kitchen!!',
           price: 3.33
+        },
+        {
+          name: 'Brownie',
+          description: 'Sweet strips of delicious funnel cake made by our special in house chef. Fresh out of the kitchen!!',
+          price: 9.99
+        },
+        {
+          name: 'Deep fry icecream',
+          description: 'Sweet strips of delicious funnel cake made by our special in house chef. Fresh out of the kitchen!!',
+          price: 4.0
+        },
+        {
+          name: 'Sweet Candy',
+          description: 'Sweet strips of delicious funnel cake made by our special in house chef. Fresh out of the kitchen!!',
+          price: 9.99
         }
       ],
       drink: [
         {
           name: 'coke',
+          size: '500ml',
+          type: 'Bottle',
+          price: 2.99
+        },
+        {
+          name: 'Diet coke',
+          size: '500ml',
+          type: 'Bottle',
+          price: 2.99
+        },
+        {
+          name: 'Orange soda coke',
+          size: '500ml',
+          type: 'Bottle',
+          price: 2.99
+        },
+        {
+          name: 'Sprite coke',
           size: '500ml',
           type: 'Bottle',
           price: 2.99
@@ -59,16 +136,72 @@ const fakeData = [
         {
           name: '10 Chicken wings',
           description: 'Sweet, spicy and Cripsy dreep fried chicken wings with our special sauce!!',
-          styleChoice: ['Breaded', 'Classic'],
+          styleChoice: {
+            hasOption: true,
+            options: ['Breaded', 'Classic']
+          },
           freeDipping: 2,
-          gluten: false
+          gluten: false,
+          price: 12.333
+        },
+        {
+          name: 'Big Box Fries',
+          description: 'Sweet, spicy and Cripsy dreep fried chicken wings with our special sauce!!',
+          styleChoice: {
+            hasOption: false,
+            options: []
+          },
+          freeDipping: 0,
+          gluten: false,
+          price: 4.55
+        },
+        {
+          name: 'Half Moon Cheesy Bread',
+          description: 'Sweet, spicy and Cripsy dreep fried chicken wings with our special sauce!!',
+          styleChoice: {
+            hasOption: false,
+            options: []
+          },
+          freeDipping: 0,
+          gluten: false,
+          price: 4.55
         }
       ],
       dipping: [
         {
           name: 'Creamy Garlic',
           description: '',
-          price: 0,
+          price: 1.44,
+          gluten: false
+        },
+        {
+          name: 'Blue Cheese',
+          description: '',
+          price: 1.44,
+          gluten: false
+        },
+        {
+          name: 'Honey Garlic',
+          description: '',
+          price: 1.44,
+          gluten: false
+        },
+        {
+          name: 'Creamy Dil',
+          description: '',
+          price: 1.44,
+          gluten: false
+        },
+        {
+          name: 'Mild',
+          description: '',
+          price: 1.44,
+          gluten: false
+        },
+        {
+          name: 'Hot',
+          description: '',
+          price: 1.44,
           gluten: false
         }
       ]
