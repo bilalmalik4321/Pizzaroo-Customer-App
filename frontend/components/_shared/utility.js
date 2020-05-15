c9a8fc5c2c0df491c62d0b6c83831f6cd14922e6
Prakort <leanprakort@gmail.com>
2020-05-15 13:41:32 -0400

    clean review screen

--- frontend/components/_shared/utility.js
+++ frontend/components/_shared/utility.js
@@ -1,8 +1,42 @@
 import moment from 'moment';
 
 /**
- * 
+ * Convert format of the date
  * @param {String} time - Local timestamp 
  * @return {String} time - Date format e.g  May 12 2020 11:45 pm
  */
 export const convertDate = (time) => moment(time,'YYYY-MM-DD hh:mm:ss a').add(1,'day').format('LLL');
+
+/**
+ * Count the total price of an order
+ * @param {Object} items - object contains orders of each kind of food
+ * @return {Number} count - total price of the order
+ */
+
+export const total = (items) => {
+  
+  let count = 0;
+  Object.keys(items).map((type) => {
+    items[type].map((element) => {
+      count = count + (element.quantity * element.price);
+    })
+  });
+
+  return count;
+}
+
+/**
+ * Count the total number of item in an ordeer
+ * @param {Object} - items - object contains orders of each kind of food
+ * @return {Number} - total items in an order
+ */
+
+export function findNumberOfOrder(items) {
+  
+  let count = 0;
+  Object.keys(items).map((type) => {
+      count = count + type.length;
+  });
+
+  return count;
+}
--- frontend/components/order/review.js
+++ frontend/components/order/review.js
@@ -1,7 +1,5 @@
-import Icon from "react-native-vector-icons/FontAwesome";
 import React, { useState, useEffect } from "react";
 import {
-  StyleSheet,
   View,
   SafeAreaView,
   TouchableOpacity,
@@ -12,135 +10,57 @@ import {
   Divider,
   ListItem
 } from "react-native-elements";
-
+import Icon from "react-native-vector-icons/FontAwesome";
 import StickyHeaderFooterScrollView from 'react-native-sticky-header-footer-scroll-view';
 import {subscribe} from "react-contextual";
 
+import { findNumberOfOrder, total } from '../_shared/utility';
 
-const total = (items) => {
-  
-  let count = 0;
-  const { pizzas, dipping, drinks, sides,desserts} = items;      
-
-  pizzas.forEach(element =>{
-    count = count + (element.quantity * element.price);
-  });
-  drinks.forEach(element =>{
-    count = count + (element.quantity * element.price);
-  });
-  sides.forEach(element =>{
-    count = count + (element.quantity * element.price);
-  });
-  dipping.forEach(element =>{
-    count = count + (element.quantity * element.price);
-  });
-  desserts.forEach(element =>{
-    count = count + (element.quantity * element.price);
-  });
-
-  return count;
-
- }
-
- function findNumberOfOrder(items) {
-  const {pizzas, desserts, drinks, dipping, sides} = items;
-
-  return pizzas.length+ desserts.length+ drinks.length+ dipping.length+sides.length
-}
-
+/**
+ * Review component that shows the list of the order and total
+ * @param {Object} props - store of HOC
+ *  
+ */
 const Review = (props) => {
 
-  const temp = {
-    pizzas: [{
-      id: '123',
-      kind: 'pizza',
-      name: 'Hawaii',
-      quantity: 2,
-      price: 22.99,
-      description: 'Best pizza eve',
-      cal: 0,
-      type: '',
-      size: 'S',
-      sizeDescription: 'Small'
-    },
-    {
-      id: '123',
-      kind: 'pizza',
-      name: 'Hawaii',
-      quantity: 2,
-      price: 22.99,
-      description: 'Best pizza eve',
-      cal: 0,
-      type: '',
-      size: 'S',
-      sizeDescription: 'Small'
-    },
-    {
-      id: '123',
-      kind: 'pizza',
-      name: 'Hawaii',
-      quantity: 1,
-      price: 22.99,
-      description: 'Best pizza eve',
-      cal: 0,
-      type: '',
-      size: 'S',
-      sizeDescription: 'Small'
-    }],
-    drinks: [{
-      id: '1234',
-      kind: 'drinks',
-      name: 'Coke',
-      quantity: 2,
-      price: 1.99,
-      description: '',
-      cal: 0,
-      type: 'Bottle',
-      size: '500 ml'
-    }],
-    dipping: [{
-      id: '12345',
-      kind: 'dipping',
-      name: 'Honey Garlic',
-      quantity: 3,
-      price: 0.99,
-      description: '',
-      cal: 0,
-      type: '',
-      size: ''
-    }],
-    sides: []
-  };
- 
+  // count the number of the items
   const numberOfOrder =findNumberOfOrder(props.items);
 
+  // if user removes all item from the list, return to the Menu
   useEffect(() => {
    if(numberOfOrder === 0)
      props.navigation.navigate("Menu")
   }, [numberOfOrder]);
 
-
+  // toggle edit x icons 
   const [edit, setEdit] = useState(false);
+  // get each kind of food
   const { pizzas, desserts, drinks, sides, dipping } = props.items;
-  // console.log("-----*****Order*****------", props.pizzaOrder);
-  console.log("-----*****Item*****------", props.items);
+
+  // Remove an item of the list
   const onRemove = (item) => {
     if(edit) {
+      // get the id of the item
       const { id } = item;
+      // get the type of the item
       const kind = item.kind;
-      console.log("itemsss", props.items, 'kinda', kind)
+      // get the current item list
       const currentList = props.items[kind];
+      // filter the current list to get a new list without the removed item
       const newList = currentList.filter( obj => obj.id != id);
-      props.updateItems(kind,newList)
+      // update the store with the new list
+      props.updateItems(kind,newList);
+      // toggle x remove icons
       setEdit(false);
     }
   }
+
   return (
     <SafeAreaView>
       <StickyHeaderFooterScrollView
        makeScrollable = {true}
        renderStickyFooter={() => 
-        <View  style={styles.shoppingButton}>
+        <View  style={{paddingLeft: 15, paddingRight:15, marginBottom: 10 }}>
           <Button 
             buttonStyle={{backgroundColor: '#ff6363', borderRadius: 20}}
             raised 
@@ -154,8 +74,6 @@ const Review = (props) => {
       >
 
         <View style={{backgroundColor: 'white'}}>
-         
-
           <View  style={{padding: 20, flex: 1, flexDirection: "row", justifyContent: 'space-between'}}>
             <View style={{paddingLeft: 15}}>
               <Text h4 style={{fontWeight: "bold"}} >
@@ -167,8 +85,8 @@ const Review = (props) => {
               onPress={()=> setEdit(!edit)}
             >
               <Text h4 style={{fontWeight: "bold" , color: "#ff6363"}} >
-                  {!edit?"Edit" : ''}
-                </Text>
+                {!edit?"Edit" : ''}
+              </Text>
             </TouchableOpacity>
             </View>
           </View>
@@ -243,7 +161,7 @@ const Review = (props) => {
           </View>
 
              {/* -------- Dipping --------- */}
-             <View style={{ paddingLeft: 20, paddingRight: 20}}>
+          <View style={{ paddingLeft: 20, paddingRight: 20}}>
             {dipping && Object.keys(dipping).length!=0 && dipping.sort((a,b)=>a.name < b.name).map((item,index)=>(
               <View  key={index}>
                 <ListItem 
@@ -259,16 +177,16 @@ const Review = (props) => {
             ))}
           </View>
 
-        <View style={{padding: 30}}>
-        <Button
+          <View style={{padding: 30}}>
+          <Button
             title="Add more items"
             titleStyle={{ color: '#ff6363', alignSelf:'center'}}
             buttonStyle={{ alignSelf: 'center', padding: 10, borderRadius: 20, backgroundColor: "white" , borderColor:'#ff6363', borderWidth: 1 }}
             onPress={()=> props.navigation.navigate("Menu")}
-        />
-        </View>
-      
-        <View  style={{padding: 20, flex: 1, flexDirection: "row", justifyContent: 'space-between'}}>
+          />
+          </View>
+        
+          <View  style={{padding: 20, flex: 1, flexDirection: "row", justifyContent: 'space-between'}}>
             <View style={{paddingLeft: 15}}>
               <Text h4 style={{fontWeight: "bold"}} >
                 Total
@@ -276,141 +194,13 @@ const Review = (props) => {
             </View>
             <View style={{paddingRight: 15}}>
               <Text h4 style={{fontSize:15, color: "#ff6363"}} >
-                  ${(total(props.items)).toFixed(2)}
-                </Text>
+                ${(total(props.items)).toFixed(2)}
+              </Text>
             </View>
-            </View>
+          </View>
         </View>
-        {/* <View style={{ alignSelf: 'center',justifyContent: 'center', alignContent: 'center', textAlign: 'center'}}>
-          <Button style={{ fontWeight: "bold", flex: 1, alignItems: 'center',justifyContent: 'flex-end', marginBottom: 20 }}
-            onPress={() => {
-            //TODO go to pick location
-            if(numberOfOrder!=0){
-              props.navigation.navigate("Checkout");
-            }
-            }}
-            title={"Checkout"}
-            buttonStyle={{...styles.foodAddOrder}}
-          />
-      </View> */}
       </StickyHeaderFooterScrollView>
-   
     </SafeAreaView>
   );
 }
 export default subscribe()(Review);
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-  },
-
-  modalView: {
-    margin: 20,
-    // height: '100%',
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: "5%",
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.6,
-    shadowRadius: 3.84,
-  },
-
-  foodAddOrder: {
-    backgroundColor: "#ff6363",
-    borderRadius: 25,
-    height: 45,
-    width: '80%',
-    marginTop: 20,
-  },
-
-  foodItemDescription: {
-    marginBottom: 10,
-  },
-
-  foodItemPrice: {
-    marginBottom: 10,
-    color: "green",
-  },
-
-  modalItemHeader: {
-    textAlign:"center",
-  },
-
-  modalInput: {
-    marginBottom: 20,
-    margin:20,
-    width:"80%",
-    height:40,
-  },
-
-  modalInput2: {
-    marginBottom: 20,
-    margin:20,
-    width:"100%",
-    height:60,
-  },
-
-  modalPrice: {
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 20,
-    color: "green",
-  },
-
-  modalExit: {
-    // left:1,
-    // padding:5,
-    position:"absolute",
-  },
-  displayChoice: { 
-    width: '100%', 
-    paddingBottom: 20, 
-    paddingLeft: 20, 
-    paddingRight: 20, 
-    marginTop:0
-  }
-    ,
-  cardborder: {
-    shadowColor: "#000",
-    shadowOffset: {
-	    width: 0,
-	    height: 1,
-    },
-    shadowOpacity: 0.20,
-    shadowRadius: 1.41,
-    elevation: 2,
-    borderRadius: 10,
-  },
-  drinks: {
-    paddingBottom: 0,
-    marginBottom: 0,
-    shadowColor: "#000",
-    shadowOffset: {
-	    width: 0,
-	    height: 1,
-    },
-    shadowOpacity: 0.20,
-    shadowRadius: 1.41,
-    elevation: 2,
-    borderRadius: 10,
-  },
-
-  shoppingButton: {
-    paddingLeft: 15,
-    paddingRight:15,
-    marginBottom: 10
-  },
-
-  Icon: {
-    marginTop:2,
-    marginRight: 10
-  }
-
-});
